b0f6d5353ef7242af3c17cc677aeb5362bdca12f
XAYRGA <dzbush25@gmail.com>
2021-03-24 22:05:49 -0400

    push initial

---
+++ arfafs/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using xayrga;
+
+namespace arfafs
+{
+    class Program
+    {
+
+        static void runTest(string filename)
+        {
+
+            Console.WriteLine($"Unit test for {filename}");
+            var inFile = filename;
+            cmdarg.assert(!File.Exists(inFile), $"Halt: {inFile} doesn't exist.");
+
+
+            var fileStream = File.OpenRead(inFile);
+            var fileReader = new BinaryReader(fileStream);
+            var afsData = AFSFile.load(fileReader);
+
+            var scene = afsData.sections[0];
+
+
+            var wl = new JSRSceneBin(scene);
+            wl.parse();
+
+
+
+
+            /*
+             * 
+             * 
+             * 
+            // Scene Parse
+            var sceneSectData = afsData.sections[0].data;
+            var sceneSectReader = new MemoryStream(sceneSectData);
+            var sceneSect = new BinaryReader(sceneSectReader);
+
+            var scenePointers = new Stack<uint>();
+            uint b = 0;
+            uint largestPointer = 0;
+            uint smallestPointer = 0xFFFFFFFF; 
+
+            while (((b = sceneSect.ReadUInt32()) & 0x8C000000) == 0x8C000000)// bruh 
+            {
+                if (!((b & 0x8C000000) == 0x8C000000))
+                    break;
+                if (b > 0x8CFFFFFF)
+                    break;
+                scenePointers.Push(b);
+                if (b > largestPointer)
+                    largestPointer = b;
+                if (b < smallestPointer)
+                    smallestPointer = b;
+
+            }
+            // UNWRAP STACK HERE 
+            var sections = new uint[scenePointers.Count];
+            for (int i = scenePointers.Count - 1; i >= 0; i--)
+                sections[i] = scenePointers.Pop();
+            //
+
+            Console.WriteLine();
+            Console.WriteLine($"Offset high = 0x{largestPointer:X}, low = {smallestPointer:X}");
+            Console.WriteLine($"Virtual allocation address = 0x8c800000");
+            */
+
+        }
+
+        static void Main(string[] args)
+        {
+            /*
+            args = new string[] {
+                "demo04.afs",
+                "out1",
+            };
+            cmdarg.cmdargs = args;
+
+            var inFile = cmdarg.assertArg(0, "AFS File");
+            var outFolder = cmdarg.assertArg(1, "Out Folder");
+            */
+
+            for (int i=2; i < 3; i++)
+            {
+                runTest($"DEMO0{i}.afs");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ arfafs/afs.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; 
+
+namespace arfafs
+{
+    public class AFSFile
+    {
+        private const int AFS_HEAD = 0x534641;
+        public int sectionCount;
+        public AFSSection[] sections;    // sections are relative to file base
+
+        public static AFSFile load(BinaryReader reader)
+        {
+            var head = reader.ReadInt32();
+            if (head != AFS_HEAD)
+                throw new InvalidDataException($"AFSFile.load expected 0x{AFS_HEAD:X} 'AFS\\x00', got {head:X}");
+            var AFS = new AFSFile();
+
+            AFS.sectionCount = reader.ReadInt32();
+            AFS.sections = new AFSSection[AFS.sectionCount];
+           
+            for (int i = 0; i < AFS.sectionCount; i++)
+                AFS.sections[i] = AFSSection.load(reader);
+
+            for (int i=0; i < AFS.sectionCount; i++)
+            {
+                reader.BaseStream.Position = AFS.sections[i].offset;
+                //Console.WriteLine($"{reader.BaseStream.Position:X}, {AFS.sections[i].length:X} ");
+                AFS.sections[i].data = reader.ReadBytes(AFS.sections[i].length);
+            }
+            // we're at the end anchor of the second section
+
+            /*
+            // locate filetable offset
+            // there's probably better way to do this
+            Console.WriteLine("Finding filetable...");
+            while ((reader.ReadByte()) == 0) ;
+
+            reader.BaseStream.Position -= 1; // Dec one pos.
+            //NNGHGHGNGHH
+            */
+            reader.BaseStream.Position = AFS.sections[0].offset - 0x10;
+            reader.ReadUInt64();
+            var filetable_offset = reader.ReadUInt32();
+           // Console.WriteLine($"{filetable_offset:X}");
+            reader.BaseStream.Position = filetable_offset;
+
+            for (int i = 0; i < AFS.sectionCount; i++)
+                AFS.sections[i].descriptor = AFSSectionDescriptor.load(reader);
+
+            return AFS;
+        }
+
+    }
+
+    public class AFSSection
+    {
+        public int offset;
+        public int length;
+        public AFSSectionDescriptor descriptor;
+        public byte[] data;
+
+        public static AFSSection load(BinaryReader reader)
+        {
+            var AFSection = new AFSSection
+            {
+                offset = reader.ReadInt32(),
+                length = reader.ReadInt32(),
+            };
+            return AFSection;
+        }
+    }
+
+
+    public class AFSSectionDescriptor
+    {
+        public string name; // 16 bytes
+        public byte[] unknown;        
+        public int un2;
+        public ushort un3;
+        public ushort un4;
+        public uint un5;
+        public uint length;
+
+        public static AFSSectionDescriptor load(BinaryReader reader)
+        {
+            var afsd = new AFSSectionDescriptor()
+            {
+                name = Encoding.ASCII.GetString(reader.ReadBytes(32)),
+                un2 = reader.ReadInt32(),
+                un3 = reader.ReadUInt16(),
+                un4 = reader.ReadUInt16(),
+                un5 = reader.ReadUInt32(),  
+                length = reader.ReadUInt32(),                
+            };
+            return afsd;
+        }
+
+    }
+
+}
